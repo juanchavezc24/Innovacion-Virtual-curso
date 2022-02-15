3926f824ee3950383c69c0e4ce74a98e0d0a6eca
juanchavezc24 <juanchavezc@gmail.com>
2022-02-14 22:41:36 -0700

    third commit

---
+++ config.py
@@ -0,0 +1,15 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except PermissionError:
+        print("You don't have enough permissions to access this file/folder")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+if __name__ == '__main__':
+    main()
